2ede944450cc1a5a449cdcde1a4306e26951f5bf
MUKUL ARORA <aruntandon.tandon007@gmail.com>
2025-09-23 00:58:47 +0530

    created model for Menu Category

--- home/models.py
+++ home/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class MenuCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Menu Category"
+        verbose_name_plural = "Menu Categories"
+
+    def __str__(self):
+        return self.name
