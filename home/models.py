a700ea04a2fc4576c2f639a69aa1f6691bb68cf1
Aditya Tandon <aruntandon.tandon007@gmail.com>
2025-09-23 01:07:28 +0530

    50555

--- home/models.py
+++ home/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class MenuCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = "Menu Category"
