8dc6663a23f7160d84787222dd6d2c2ed5c5d049
Aditya Tandon <aruntandon.tandon007@gmail.com>
2025-09-24 00:39:27 +0530

    50557

--- orders/models.py
+++ orders/models.py
@@ -3,4 +3,10 @@ from django.db import models
 # Create your models here.
 class OrderStatus(models.Model):
     name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Order Statuses"
+
+    def __str__(self):
+        self.name
     
